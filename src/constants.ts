0b637757daefa3221c9b1651b25f8719c03e762b
Jarema <melgaer@gmail.com>
2018-08-15 21:44:51 +0200

    fix typo in constants

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@ import * as env from 'env-var';
 
 export const MONGO_URL = env.get('MONGO_URL');
 export const MONGO_USER = env.get('MONGO_USER');
-export const MONGO_PASSWORD = env.get('MONGO_PASSWORD';)
+export const MONGO_PASSWORD = env.get('MONGO_PASSWORD');
