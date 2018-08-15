8535587bb2e4db89fb5013a19fb960847bd3042e
Jarema <melgaer@gmail.com>
2018-08-15 21:44:11 +0200

    use envs for db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from 'app.controller';
 import { AppService } from 'app.service';
-import { KidsController } from 'kids/kids.controller';
-import { KidsService } from 'kids/kids.service';
 import { KidsModule } from 'kids/kids.module';
 
 @Module({
--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,9 +1,10 @@
 import * as mongoose from 'mongoose';
+import * as con from '../constants';
 
 export const databaseProviders = [
   {
     provide: 'DbConnectionToken',
     useFactory: async (): Promise<typeof mongoose> =>
-      await mongoose.connect('mongodb://kids:kids@mongo-mongodb.default.svc.cluster.local/kids'),
+      await mongoose.connect(`mongodb://${con.MONGO_USER}:${con.MONGO_PASSWORD}@${con.MONGO_URL}/kids`),
   },
 ];
