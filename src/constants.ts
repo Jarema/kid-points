c2b13948ed7b013433830d21608fe43572012052
Jarema <melgaer@gmail.com>
2018-08-16 08:25:22 +0200

    fix constant declaration syntax

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 import * as env from 'env-var';
 
-export const MONGO_URL = env.get('MONGO_URL').required().asString;
-export const MONGO_USER = env.get('MONGO_USER').required().asString;
-export const MONGO_PASSWORD = env.get('MONGO_PASSWORD').required().asString;
+export const MONGO_URL = env.get('MONGO_URL').required().asString();
+export const MONGO_USER = env.get('MONGO_USER').required().asString();
+export const MONGO_PASSWORD = env.get('MONGO_PASSWORD').required().asString();
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from 'app.module';
 
 async function bootstrap() {
-  console.log("test: ", process.env.TEST_ENV)
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
